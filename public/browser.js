58031e9bfe91118706cfd05258ab95f2aede7154
bodya17 <levantovychbogdan@gmail.com>
2017-06-10 23:22:51 +0300

    two boards

--- public/browser.js
+++ public/browser.js
@@ -1,23 +1,42 @@
-// var socket = new WebSocket("ws://dead-simple-chess.herokuapp.com");
-// // var socket = new WebSocket("ws://localhost:3000");
-
+var chess = new Chess();
+var chess2 = new Chess();
 
 var socket = io()
 
-var onChange = function(oldPos, newPos) {
-  socket.emit('newPos', ChessBoard.objToFen(newPos));
-};
+var onDrop1 = function(from, to) {
+  socket.emit('moveBoard1', { from, to })
+}
+
+var onDrop2 = function(from, to) {
+  socket.emit('moveBoard2', { from, to })
+}
 
 var cfg = {
   draggable: true,
   position: 'start',
-  onChange: onChange
-};
+  onDrop: onDrop1,
+}
 
-var board = ChessBoard('board', cfg);
+var cfg2 = {
+  draggable: true,
+  position: 'start',
+  onDrop: onDrop2,
+}
 
-socket.on('newPos', function(newPos) {
-  board.position(newPos)
+var board = ChessBoard('board', cfg)
+var board2 = ChessBoard('board2', cfg)
+
+socket.on('moveBoard1', move => {
+  chess.move(move)
+  board.position(chess.fen())
 })
 
+socket.on('moveBoard2', move => {
+  chess2.move(move)
+  board2.position(chess2.fen())
+})
 
+function putPawn(where) {
+  chess.put({ type: chess.PAWN, color: chess.BLACK }, where)
+  board.position(chess.fen())
+}
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 app.use(express.static('public'))
 
 io.on('connection', function(socket){
     socket.on('newPos', function(newPos) {
-        io.emit('newPos', newPos);
-  })
-});
+      io.emit('newPos', newPos)
+    })
+    socket.on('move', function(move) {
+        console.log('new move')
+        io.emit('move', move)
+    })
+})
 
-http.listen( process.env.PORT || 3000, function(){
-});
+http.listen( process.env.PORT || 3000, function() {
+    console.log('Server listenting on port 3000')
+})
